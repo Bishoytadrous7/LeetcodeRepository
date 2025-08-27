89e8020a5f2b745db0828922a9b666eb99c66927
Bishoy Tadrous <bntadrous@knox.edu>
2025-08-27 20:24:17 +0000

    edit

--- longestRepeatingCharacterReplacement.py
+++ longestRepeatingCharacterReplacement.py
@@ -1,21 +1,30 @@
 class Solution:
-  def characterReplacement(self, s: str, k: int) -> int:
-      count = {}
-      l = 0
-      res = 0
+    def characterReplacement(self, s: str, k: int) -> int:
+        '''
+        Q: empty string?
+        Q: time/space constraints?
 
-      for r in range(len(s)):
-          count[s[r]] = 1+ count.get(s[r], 0)
+        # Approach 1: Brute Force O(n^2)
+        check every possible substring
 
-          while (r-l+1) - max(count.values()) > k:
-              count[s[l]] -= 1
-              l+=1
-          res = max(res, r-l+1)
-      return res
-
-#Time complexity: O(n * 26)
-#Space Complexity: O(n)
-
+        # Approach 2: Sliding Window O(n)
+        validation criteria: length of substring - max char frequency <= k
+        move right pointer until validation criteria is not met
+        move left pointer until validation criteria is met
+        keep track of alphabets count in a dictionary
+        '''
+        l = 0
+        res = 0
+        counts = {}
+        for r in range(len(s)):
+            counts[s[r]] = counts.get(s[r], 0) + 1
+            while (r - l + 1) - max(counts.values()) > k:
+                counts[s[l]] -= 1
+                l += 1
+            res = max(r - l + 1, res)
+        return res
+#Time: O(26 *n) = O(n)
+#Space:O(1)
 
 #Better Approach
 class Solution:
